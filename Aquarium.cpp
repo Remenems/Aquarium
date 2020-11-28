7a09222fbacdf2784b711b8333a2aa67c8580b26
Remenems <cazinremi@gmail.com>
2020-11-28 12:19:08 +0100

    Ajout des constantes du milieu dans Aquarium

--- Aquarium.cpp
+++ Aquarium.cpp
@@ -6,8 +6,8 @@
 Aquarium::Aquarium( int width, int height, int _delay ) : CImgDisplay(), delay( _delay )
 {
 
-   int         screenWidth = 1280; //screen_width();
-   int         screenHeight = 1024; //screen_height();
+   int screenWidth = 1280; //screen_width();
+   int screenHeight = 1024; //screen_height();
 
 
    cout << "const Aquarium" << endl;
--- Aquarium.h
+++ Aquarium.h
@@ -20,6 +20,17 @@ private :
 
    int delay;
 
+   static const tuple<float,float> plageChampAngulaireVision;
+   static const tuple<float,float> plageDistanceVision;
+   static const tuple<float,float> plageDistanceOuie;
+   static const tuple<float,float> plageCapaciteDetectionOuie;
+   static const tuple<float,float> plageCamouflage;
+   static const tuple<float,float> protectionCarapacemax;
+
+   static const float vitesseNageoireMax;
+   static const float probaMaxClonage;
+   static const int seuilAvantPeurMax;
+
 public :
    Aquarium( int width, int height, int _delay );
    ~Aquarium( void );
