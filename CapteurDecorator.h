a897dd1731db3c2746041fbcdfc7966fd4d89fe6
Remenems <cazinremi@gmail.com>
2020-12-04 16:01:22 +0100

    Ajout des classes pour le capteur

---
+++ CapteurDecorator.h
@@ -0,0 +1,9 @@
+#ifndef _CAPTEUR_DECORATOR_H_
+#define _CAPTEUR_DECORATOR_H_
+
+class CapteurDecorator
+{
+//TODO classe capteur decorator
+};
+
+#endif
---
+++ Oreille.h
@@ -0,0 +1,10 @@
+#ifndef _OREILLE_H_
+#define _OREILLE_H_
+
+class Oreille
+{
+//TODO Classe oreille
+};
+
+
+#endif
