d4690318c8e5c01c1682c0829636cbcec7862ae4
Remenems <cazinremi@gmail.com>
2020-12-04 11:01:57 +0100

    Ajout du #define

--- IPrototypeBestiole.h
+++ IPrototypeBestiole.h
@@ -1,3 +1,6 @@
+#ifndef _PROTOTYPE_BEST_H_
+#define _PROTOTYPE_BEST_H_
+
 /* interface du design pattern Prototype */
 
 class IPrototypeBestiole
@@ -5,3 +8,5 @@ class IPrototypeBestiole
     public:
     virtual IPrototypeBestiole clone() = 0;
 };
+
+#endif _PROTOTYPE_BEST_H_
